d4c39325d26669ef97980ac20a32571393fe2723
Emerson Lima <emersondll@outlook.com>
2026-04-28 19:59:13 -0300

    feat: add jacoco coverage check rule to enforce 80% instruction threshold in pom.xml

--- src/main/java/io/github/emersondll/auth/enumerations/SchedulingStatus.java
+++
@@ -1,8 +0,0 @@
-package io.github.emersondll.auth.enumerations;
-
-public enum SchedulingStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELED,
-    COMPLETED
-}
--- src/main/java/io/github/emersondll/auth/exception/SchedulingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.github.emersondll.auth.exception;
-
-
-public class SchedulingNotFoundException extends RuntimeException {
-    public SchedulingNotFoundException(String id) {
-        super("Scheduling not found with id: " + id);
-    }
-}
